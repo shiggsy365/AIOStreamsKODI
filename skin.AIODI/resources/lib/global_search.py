89e0c017f8f092b52412ed00267de67d3b008b5e
shiggsy365 <shiggsy365@gmail.com>
2026-01-15 22:02:15 +0000

    Repository Update: Plugin 1.1.154, Skin 6.0.372

--- skin.AIODI/resources/lib/global_search.py
+++ skin.AIODI/resources/lib/global_search.py
@@ -14,21 +14,32 @@ try:
     # Try to get the window
     dialog = xbmcgui.Window(current_dialog_id)
     
-    # Get the search query from arguments or the edit control
+    # Get the search query from arguments, the edit control, or keyboard
     search_query = None
     if len(sys.argv) > 1:
         search_query = sys.argv[1]
         xbmc.log('[Global Search] Query provided via argument: %s' % search_query, xbmc.LOGINFO)
-        # Populate the edit control so the user sees it
+        # Populate the edit control so the user sees it if the dialog is open
+        if current_dialog_id == 1106:
+            try:
+                edit_control = dialog.getControl(9000)
+                edit_control.setText(search_query)
+            except:
+                pass
+    elif current_dialog_id == 1106:
+        # Get the search query from the edit control in the search dialog
         try:
             edit_control = dialog.getControl(9000)
-            edit_control.setText(search_query)
+            search_query = edit_control.getText()
         except:
             pass
-    else:
-        # Get the search query from the edit control
-        edit_control = dialog.getControl(9000)
-        search_query = edit_control.getText()
+    
+    # If no query yet, open keyboard
+    if not search_query:
+        keyboard = xbmc.Keyboard('', 'Global Search')
+        keyboard.doModal()
+        if keyboard.isConfirmed():
+            search_query = keyboard.getText()
     
     xbmc.log('[Global Search] Final Search query: %s' % search_query, xbmc.LOGINFO)
     
@@ -42,6 +53,7 @@ try:
         # Set properties on home window
         home_window = xbmcgui.Window(10000)
         home_window.setProperty('GlobalSearch.Query', search_query)
+        # Default to movies tab if not set or verify active tab
         home_window.setProperty('GlobalSearch.ActiveTab', 'movies')
         home_window.setProperty('GlobalSearch.MoviesURL', movie_url)
         home_window.setProperty('GlobalSearch.SeriesURL', series_url)
@@ -52,11 +64,16 @@ try:
         xbmc.log('[Global Search] Movies URL: %s' % movie_url, xbmc.LOGINFO)
         xbmc.log('[Global Search] Series URL: %s' % series_url, xbmc.LOGINFO)
         
-        # Close search dialog and open results
-        xbmc.executebuiltin('Dialog.Close(1106)')
+        # Close search dialog if open and open results
+        if current_dialog_id == 1106:
+            xbmc.executebuiltin('Dialog.Close(1106)')
+            
         xbmc.executebuiltin('ActivateWindow(1112)')
     else:
-        xbmcgui.Dialog().notification('Search', 'Please enter a search term', xbmcgui.NOTIFICATION_WARNING, 2000)
+        # Only notify if we were expecting an argument or valid input; 
+        # canceling keyboard is a valid "do nothing" action.
+        if len(sys.argv) > 1:
+             xbmcgui.Dialog().notification('Search', 'Please enter a search term', xbmcgui.NOTIFICATION_WARNING, 2000)
 except Exception as e:
     xbmc.log('[Global Search] Error: %s' % str(e), xbmc.LOGERROR)
     xbmcgui.Dialog().notification('Search Error', str(e), xbmcgui.NOTIFICATION_ERROR, 3000)
